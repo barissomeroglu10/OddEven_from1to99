bae447faf722604d97cbce6a7894e1d94e024934
baris <baris@BARIS_SOMEROGLU>
2023-12-31 22:49:18 +0300

    Add project files.

---
+++ OddEven_from1to99/OddEven_from1to99.cpp
@@ -0,0 +1,41 @@
+﻿/*
+In this code, we will figure total and multiplication for odd and even numbers from 1 to 99
+and write them on the screen.
+
+Developer: Barış Someroğlu
+Date: 31.12.2023
+*/
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int Number, OddTotal = 0, OddMultiplication = 1, EvenTotal = 0, EvenMultiplication = 1;
+	int i;
+
+	for (i = 1; i <= 99; i++)
+	{
+		if (i % 2 == 0)
+		{
+			EvenTotal += i;
+			EvenMultiplication *= i;
+		}
+
+		else
+		{
+			OddTotal += i;
+			OddMultiplication += i;
+		}
+	}
+
+	cout << "Total for Odd Numbers from 1 to 99: " << OddTotal << "\n";
+	cout << "Multiplication for Odd Numbers from 1 to 99: " << OddMultiplication << "\n";
+
+	cout << "Total for Even Numbers from 1 to 99: " << EvenTotal << "\n";
+	cout << "Multiplication for Even Numbers from 1 to 99: " << EvenMultiplication << "\n";
+
+	return 0;
+}
